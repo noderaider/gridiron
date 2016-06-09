4a40fd53ed4902b5efd045e8bcd75f157ed5a519
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-06-08 23:19:54 -0700

    grids sizing to children

--- src/lib/components/react-virtualized/createGrid.js
+++ src/lib/components/react-virtualized/createGrid.js
@@ -10,13 +10,14 @@ export default function createGrid({ React, connect, ReactVirtualized, Immutable
   should.exist(React)
   should.exist(connect)
   should.exist(ReactVirtualized)
-  const {Component, PropTypes} = React
+  const {Component, PropTypes, cloneElement} = React
   const {AutoSizer, FlexTable, FlexColumn, SortDirection, SortIndicator, Grid} = ReactVirtualized
   const Expander = createExpander({ React })
 
 
 
 
+  const pollingFrequency = 1000
   class ReduxGrid extends Component {
     static propTypes = createPropTypes(React);
     static defaultProps = { maxHeight: 800
@@ -27,7 +28,7 @@ export default function createGrid({ React, connect, ReactVirtualized, Immutable
       this.state = {
         disableHeader: false,
         headerHeight: 30,
-        height: 270,
+        height: 600,
         hideIndexRow: false,
         overscanRowCount: 10,
         rowHeight: 40,
@@ -38,8 +39,18 @@ export default function createGrid({ React, connect, ReactVirtualized, Immutable
         useDynamicRowHeight: false
       }
     }
+    /*
+    componentDidMount() {
+      this.measureID = setInterval(this._measure, pollingFrequency)
+    }
+    componentWillUnmount() {
+      clearInterval(this.measureID)
+    }
+    _measure = () => {
+    };
+    */
     render() {
-      const { state, selectColumns, selectRows, maxHeight, styles, expandedRowManager } = this.props
+      const { state, selectColumns, selectRows, maxHeight, styles, expandRowManager } = this.props
       //const { list } = state
 
       const { disableHeader
@@ -55,7 +66,7 @@ export default function createGrid({ React, connect, ReactVirtualized, Immutable
             , useDynamicRowHeight
             } = this.state
 
-      const cellRangeRenderer = createExpandableCellRangeRenderer({ React, Expander, expandedRowManager, state })
+      const cellRangeRenderer = createExpandableCellRangeRenderer({ React, ReactCSSTransitionGroup, Expander, expandRowManager, state })
 
       should.exist(selectColumns)
       should.exist(selectRows)
@@ -107,10 +118,115 @@ export default function createGrid({ React, connect, ReactVirtualized, Immutable
                       */
                     }
                   }
-                  rowHeight={({ index }) => index === 0 ? 50 : 25}
+                  rowHeight={({ index }) => index === 0 ? 50 : 19}
                   columnCount={columnKeys.length}
                   rowCount={rowCount}
-                  cellRangeRenderer={cellRangeRenderer}
+
+                  cellRangeRenderer={
+                    ({ cellCache, cellRenderer, columnSizeAndPositionManager, columnStartIndex, columnStopIndex, horizontalOffsetAdjustment, isScrolling, rowSizeAndPositionManager, rowStartIndex, rowStopIndex, scrollLeft, scrollTop, verticalOffsetAdjustment } = {}) => {
+                      const expandedIndices = expandRowManager.getExpandedIndices()
+                      const renderedRows = []
+                      for (let rowIndex = rowStartIndex; rowIndex <= rowStopIndex; rowIndex++) {
+                        const renderedCells = []
+                        let rowDatum = rowSizeAndPositionManager.getSizeAndPositionOfCell(rowIndex)
+
+                        let isExpandable = expandRowManager.isExpandable(rowIndex)
+                        let isExpanded = expandedIndices.includes(rowIndex)
+
+                        let expanderOffset = 0
+
+
+                        /** STATIC HEIGHT ELEMENT */
+                        if(!isExpandable) {
+                          const expanderKey = `${rowIndex}-noexpander`
+                          renderedCells.push(<div key={expanderKey} />)
+                        } else {
+                          const expanderKey = `${rowIndex}-expander`
+                          expanderOffset = expandRowManager.getExpanderWidth(rowIndex)
+                          let expander = (
+                            <div
+                              key={expanderKey}
+                              style={ { height: rowDatum.size
+                                      , left: horizontalOffsetAdjustment
+                                      , width: expanderOffset
+                                      } }
+                              className={expandRowManager.getExpanderClassName(rowIndex)}
+                            >
+                              <Expander expanded={isExpanded} handleExpand={() => expandRowManager.onToggleExpand(rowIndex)}/>
+                            </div>
+                          )
+                          renderedCells.push(expander)
+                        }
+
+                        for (let columnIndex = columnStartIndex; columnIndex <= columnStopIndex; columnIndex++) {
+                          let columnDatum = columnSizeAndPositionManager.getSizeAndPositionOfCell(columnIndex)
+
+                          let key = `${rowIndex}-${columnIndex}`
+                          let renderedCell
+
+                          // Avoid re-creating cells while scrolling.
+                          // This can lead to the same cell being created many times and can cause performance issues for "heavy" cells.
+                          // If a scroll is in progress- cache and reuse cells.
+                          // This cache will be thrown away once scrolling completes.
+                          if (isScrolling) {
+                            if (!cellCache[key]) {
+                              cellCache[key] = cellRenderer({
+                                columnIndex,
+                                isScrolling,
+                                rowIndex
+                              })
+                            }
+                            renderedCell = cellCache[key]
+                          // If the user is no longer scrolling, don't cache cells.
+                          // This makes dynamic cell content difficult for users and would also lead to a heavier memory footprint.
+                          } else {
+                            renderedCell = cellRenderer({
+                              columnIndex,
+                              isScrolling,
+                              rowIndex
+                            })
+                          }
+
+                          if (renderedCell == null || renderedCell === false)
+                            continue
+
+
+                          /** STATIC HEIGHT ELEMENT */
+                          let child = (
+                            <div
+                              key={key}
+                              className='Grid__cell'
+                              style={ { height: rowDatum.size
+                                      , left: isExpandable && columnIndex === 0 ? columnDatum.offset + horizontalOffsetAdjustment + expanderOffset : columnDatum.offset + horizontalOffsetAdjustment
+                                      , width: isExpandable && columnIndex === 0 ? columnDatum.size - expanderOffset : columnDatum.size
+                                      } }
+                            >
+                              {renderedCell}
+                            </div>
+                          )
+
+                          renderedCells.push(child)
+                        }
+
+
+                        const rowStyle =  { height: rowDatum.size
+                                          //, clear: 'both'
+                                          }
+
+                        renderedRows.push(<div key={`${rowIndex}-row`} id={`${rowIndex}-row`} className={expandRowManager.rowStyle} data-row-height={rowDatum.size} style={rowStyle}>{renderedCells}</div>)
+
+                        if(isExpanded) {
+                          let key=`${rowIndex}-expanded`
+                          //let height=expandRowManager.getHeight(rowIndex)
+                          let expanded = cloneElement(expandRowManager.getContent(rowIndex, state), { key, id:key, className: 'drill' })
+                          //let expandedTransition = <SlideRow key={`${key}-transition`}>{expanded}</SlideRow>
+                          renderedRows.push(expanded)
+                        }
+                      }
+                      return renderedRows
+                    }
+                  }
+
                   cellRenderer={
                     ({ columnIndex, rowIndex, isScrolling }) => {
                       if(rowIndex === 0) {
@@ -124,6 +240,20 @@ export default function createGrid({ React, connect, ReactVirtualized, Immutable
             </AutoSizer>
           </div>
         </ContentBox>
+      )
+    }
+  }
+  class SlideRow extends Component {
+    render() {
+      const { children } = this.props
+      return (
+        <ReactCSSTransitionGroup
+          transitionName="rowSlide"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
+        >
+          {children}
+        </ReactCSSTransitionGroup>
       )
     }
   }
