983d7d3e166df8398ecd943c27e3958dbb9ec2c3
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-06-24 19:33:25 -0700

    fixed issue with modules directory in gitignore

---
+++ packages/redux-grid-example/src/lib/redux/modules/redux-idle-monitor.js
@@ -0,0 +1,30 @@
+import configure  from 'redux-idle-monitor'
+import { push } from 'react-router-redux'
+
+import { packageName, title, subtitle, IS_BROWSER } from '../../../config'
+import { setText } from '../actions/visual'
+
+export const IDLESTATUS_INACTIVE = 'INACTIVE'
+
+export const IDLE_STATUSES =  [ IDLESTATUS_INACTIVE
+                              ]
+
+
+export const idleStatusDelay = idleStatus => (dispatch, getState) => 30000
+
+
+export const activeStatusAction = (dispatch, getState) =>  {
+  dispatch(setText({ subtitle }))
+}
+
+export const idleStatusAction = idleStatus => (dispatch, getState) => {
+  dispatch(setText({ subtitle }))
+}
+
+const opts = { appName: title, IDLE_STATUSES, idleStatusDelay, activeStatusAction, idleStatusAction }
+
+const { reducer, actions, middleware } = configure(opts)
+const { start, stop, gotoIdleStatus } = actions
+export default reducer
+export { actions, middleware }
+export { start, stop, gotoIdleStatus }
