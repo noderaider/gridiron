93999d5ec814ff91dd8edf997e2c686a86f21eb5
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-07-14 07:43:55 -0700

    postjs

---
+++ packages/postjs/src/lib/index.js
@@ -0,0 +1,11 @@
+/**
+ * MIT License http://www.opensource.org/licenses/mit-license.php
+ * Author Cole Chamberlain <cole.chamberlain@gmail.com> @noderaider
+ */
+
+import path from 'path'
+import wrapper from './utils/wrapper'
+const wrap = wrapper('postjs/lib/index.js')
+
+
+/** Code go here */
