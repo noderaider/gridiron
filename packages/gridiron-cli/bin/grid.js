dd1f6753c5d7ded49a351e7ad7d6bdc812182cd6
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-08-15 14:33:12 -0700

    fixed deps and lerna beta 26

---
+++ packages/gridiron-cli/bin/grid.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+
+console.warn('NO CODE WRITTEN FOR THE CLI YET, PUT THAT HERE!');
--- packages/gridiron-cli/src/bin/grid.js
+++ packages/gridiron-cli/src/bin/grid.js
@@ -1,1 +1,3 @@
+#!/usr/bin/env node
+
 console.warn('NO CODE WRITTEN FOR THE CLI YET, PUT THAT HERE!')
